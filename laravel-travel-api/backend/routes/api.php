816d6d5cf0de09cefdcaf2e7a824eb2561409d1d
LAES18 <laes18@users.noreply.github.com>
2026-08-28 22:10:18 -0600

    feat: configure Passport authentication

--- laravel-travel-api/backend/routes/api.php
+++ laravel-travel-api/backend/routes/api.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/registro', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware('auth:api')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::apiResource('viajes', ViajeController::class);
     Route::get('/fotos', [FotoController::class, 'search']);
